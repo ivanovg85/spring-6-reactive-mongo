a2a9d30801fc2dd60016e5557d573c0be012d6d6
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-10-05 21:07:46 +0300

    initial commit

---
+++ src/main/java/art/cookedincode/reactivemongo/model/BeerDTO.java
@@ -0,0 +1,28 @@
+package art.cookedincode.reactivemongo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
