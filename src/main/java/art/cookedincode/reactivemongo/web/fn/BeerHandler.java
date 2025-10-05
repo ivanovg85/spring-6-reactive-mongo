b4324ad92ea7993e514204a339ca8a9e303e2814
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-10-06 00:55:24 +0300

    add endpoint and test for beer

---
+++ src/main/java/art/cookedincode/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,91 @@
+package art.cookedincode.reactivemongo.web.fn;
+
+import art.cookedincode.reactivemongo.model.BeerDTO;
+import art.cookedincode.reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebInputException;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+
+    private final BeerService beerService;
+    private final Validator validator;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        Flux<BeerDTO> flux;
+
+        if (request.queryParam("beerStyle").isPresent()) {
+            flux = beerService.findByBeerStyle(request.queryParam("beerStyle").get());
+        } else {
+            flux = beerService.listBeers();
+        }
+
+        return ServerResponse.ok()
+                .body(flux, BeerDTO.class);
+    }
+
+    public Mono<ServerResponse> getBeerById(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.getBeerById(request.pathVariable("beerId"))
+                                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND))),
+                        BeerDTO.class);
+    }
+
+    public Mono<ServerResponse> createBeer(ServerRequest request) {
+        return beerService.saveBeer(request.bodyToMono(BeerDTO.class).doOnNext(this::validate))
+                .flatMap(beerDTO -> ServerResponse
+                        .created(UriComponentsBuilder
+                                .fromUriString(BeerRouterConfig.BEER_PATH_ID)
+                                .build(beerDTO.getId()))
+                        .build());
+    }
+
+    public Mono<ServerResponse> updateBeerById(ServerRequest request) {
+        return request.bodyToMono(BeerDTO.class)
+                .doOnNext(this::validate)
+                .flatMap(beerDTO -> beerService
+                        .updateBeer(request.pathVariable("beerId"), beerDTO))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .flatMap(_ -> ServerResponse.noContent().build());
+    }
+
+    public Mono<ServerResponse> patchBeerById(ServerRequest request) {
+        return request.bodyToMono(BeerDTO.class)
+                .doOnNext(this::validate)
+                .flatMap(beerDTO -> beerService
+                        .patchBeer(request.pathVariable("beerId"), beerDTO))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .flatMap(_ -> ServerResponse.noContent().build());
+    }
+
+    public Mono<ServerResponse> deleteBeerById(ServerRequest request) {
+        return beerService.getBeerById(request.pathVariable("beerId"))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .flatMap(beerDTO -> beerService.deleteBeerById(beerDTO.getId()))
+                .then(ServerResponse.noContent().build());
+    }
+
+    private void validate(BeerDTO beerDTO) {
+        Errors errors = new BeanPropertyBindingResult(beerDTO, "beerDto");
+        validator.validate(beerDTO, errors);
+
+        if (errors.hasErrors()) {
+            throw new ServerWebInputException(errors.toString());
+        }
+    }
+}
---
+++ src/test/java/art/cookedincode/reactivemongo/web/fn/BeerEndpointTest.java
@@ -0,0 +1,193 @@
+package art.cookedincode.reactivemongo.web.fn;
+
+import art.cookedincode.reactivemongo.domain.Beer;
+import art.cookedincode.reactivemongo.model.BeerDTO;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static art.cookedincode.reactivemongo.services.BeerServiceImplTest.getTestBeer;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+
+
+/**
+ * Created by Georgi Ivanov
+ */
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerEndpointTest {
+
+    @Container
+    @ServiceConnection
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testListBeers() {
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()").value(greaterThan(1));
+    }
+
+    @Test
+    void testListBeersByStyle() {
+        final String BEER_STYLE = "TEST";
+        BeerDTO testDto = getSavedTestBeer();
+        testDto.setBeerStyle(BEER_STYLE);
+
+        webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(testDto), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange();
+
+        webTestClient.get().uri(UriComponentsBuilder
+                        .fromUriString(BeerRouterConfig.BEER_PATH)
+                        .queryParam("beerStyle", BEER_STYLE).build().toUri())
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()").value(equalTo(1));
+    }
+
+    @Test
+    @Order(2)
+    void testGetBeerById() {
+        BeerDTO beerDTO = getSavedTestBeer();
+
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(BeerDTO.class);
+    }
+
+    @Test
+    void testGetBeerByIdNotFound() {
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH_ID, 999)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testCreateBeer() {
+        webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().exists("location");
+    }
+
+    @Test
+    void testCreateBeerBadData() {
+        Beer testBeer = getTestBeer();
+        testBeer.setBeerName("");
+
+        webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(3)
+    void testUpdateBeer() {
+        BeerDTO beerDTO = getSavedTestBeer();
+        beerDTO.setBeerName("New Name");
+
+        webTestClient.put().uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    void testUpdateBeerNotFound() {
+        webTestClient.put().uri(BeerRouterConfig.BEER_PATH_ID, 999)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testPatchBeerNotFound() {
+        BeerDTO beerDTO = getSavedTestBeer();
+        beerDTO.setBeerName("New Name");
+
+        webTestClient.patch().uri(BeerRouterConfig.BEER_PATH_ID, 999)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateBeerBadRequest() {
+        Beer testBeer = getTestBeer();
+        testBeer.setBeerName("");
+
+        webTestClient.put().uri(BeerRouterConfig.BEER_PATH_ID, 1)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(999)
+    void testDeleteBeer() {
+        BeerDTO beerDTO = getSavedTestBeer();
+
+        webTestClient.delete().uri(BeerRouterConfig.BEER_PATH_ID, beerDTO.getId())
+                .exchange()
+                .expectStatus()
+                .isNoContent();
+    }
+
+    @Test
+    void testDeleteBeerNotFound() {
+        webTestClient.delete().uri(BeerRouterConfig.BEER_PATH_ID, 999)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+
+    public BeerDTO getSavedTestBeer() {
+        FluxExchangeResult<BeerDTO> beerDTOFluxExchangeResult = webTestClient.post().uri(BeerRouterConfig.BEER_PATH)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .returnResult(BeerDTO.class);
+
+        List<String> location = beerDTOFluxExchangeResult.getResponseHeaders().get("Location");
+
+        return webTestClient.get().uri(BeerRouterConfig.BEER_PATH)
+                .exchange().returnResult(BeerDTO.class).getResponseBody().blockFirst();
+    }
+}
